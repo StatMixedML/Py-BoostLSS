5929a507cd0f4860932c1f93ab08274d24a9cf77
Alexander März <alex.maerz@gmx.net>
2022-12-02 12:40:00 +0100

    Update __init__.py

--- pyboostlss/distributions/__init__.py
+++ pyboostlss/distributions/__init__.py
@@ -1,3 +1,5 @@
 """Py-BoostLSS - An extension of Py-Boost to probabilistic modelling"""
 
+from pyboostlss.distributions.distribution_loss_metric import *
 from pyboostlss.distributions.MVN import *
+
