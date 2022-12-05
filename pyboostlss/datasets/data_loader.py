a90e89891b280080cbe1da242726c3dc4c89ea6d
Alexander März <alex.maerz@gmx.net>
2022-12-05 10:48:40 +0100

    Create data_loader.py

---
+++ pyboostlss/datasets/data_loader.py
@@ -0,0 +1,11 @@
+import pkg_resources
+import pandas as pd
+
+
+def load_simulated_data(dta_name: str) -> pd.DataFrame:
+    """Returns dataframe of a sepecified simulated dataset example.
+    """
+    data_path = pkg_resources.resource_stream(__name__, dta_name)
+    data_df = pd.read_csv(data_path)
+
+    return data_df
