66de807cfd7bd12419e27b761419685787bcf0a3
Alexander März <alex.maerz@gmx.net>
2022-12-08 16:18:23 +0100

    Update DIRICHLET.py

--- pyboostlss/distributions/DIRICHLET.py
+++ pyboostlss/distributions/DIRICHLET.py
@@ -72,7 +72,7 @@ class DIRICHLET:
             requires_grad: bool(), Whether or not tensor requires gradient for automatic differentiation
             
         Returns:
-        target, predt, predt_location, predt_tril, nll
+        predt, nll
         """
         
         ###
