0ab99ea6e16ec3083424d4c60b9a66d759c5df75
Alexander März <alex.maerz@gmx.net>
2022-12-15 09:09:02 +0100

    Update MVN.py

--- pyboostlss/distributions/MVN.py
+++ pyboostlss/distributions/MVN.py
@@ -141,7 +141,7 @@ class MVN:
     
 
     def get_params_nll(self, y_true: cp.ndarray, y_pred: cp.ndarray, requires_grad=False) -> torch.tensor:
-        """ Returns target and estimated parameters, where parameters are transformed to the output scale
+        """ Returns estimated parameters and nll, where parameters are transformed to the output scale.
         
         Args:
             y_true: cp.ndarray, Input target variables
